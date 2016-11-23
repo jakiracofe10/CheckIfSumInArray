e0cdbeb8ca00e4ff15eb14362d4104707983294b
Jason <jakiracofe10@gmail.com>
2016-11-22 23:23:16 -0500

    "better version"

--- main.cpp
+++ main.cpp
@@ -6,17 +6,18 @@ using namespace std;
 //Time = O(n^2) -> checking each index by every other index
 //Time = O(nlogn) -> using hash map find
 
-const int MAX=65535;
-
 bool checkForSumOfXBetter(int *arr, int n, int x) {
     map<int,int> elements;
     for (int i=0; i<n; i++) {
         elements[arr[i]]++;
     }
     for (int i=0; i<n; i++) {
-        if (elements.find(x-arr[i])->first + arr[i] == x) {
-            if (elements.find(x-arr[i])->first == arr[i]) {
-                if (elements.find(x-arr[i])->second > 1) {
+        map<int,int>::iterator it = elements.find(x-arr[i]);
+        if (it == elements.end())
+            continue;
+        if (it->first + arr[i] == x) {
+            if (it->first == arr[i]) {
+                if (it->second > 1) {
                     return true;
                 }
             } else {
@@ -41,7 +42,7 @@ bool checkForSumOfX(int *arr, int n, int x) {
 int main() {
     int n = 8; //# of elements in array
     int A[n] = {1,2,3,4,5,4,4,5};
-    int x = 11;
+    int x = 3;
     bool result = checkForSumOfXBetter(A,n,x);
     cout << to_string(result);
     return 0;
